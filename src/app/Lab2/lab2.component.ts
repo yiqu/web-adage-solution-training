f2a0a8d3c3063b40a94af2c05a84bfe0cdf99bfe
Kevin Qu <kevinqu88@gmail.com>
2017-03-13 13:50:58 -0400

    Lab2 part

--- src/app/Lab2/lab2.component.ts
+++ src/app/Lab2/lab2.component.ts
@@ -1,5 +1,11 @@
 import {Component} from '@angular/core';
 
+
+/*
+ * Lab 2 - Intro to TypeScript
+ * Class and inheritance
+ * Interfaces
+*/
 @Component({
   selector: 'lab2',
   templateUrl: './lab2.component.html'
@@ -10,10 +16,26 @@ export class Lab2Component {
   private name: string = "Billy";
   private age: number = 10;
 
+  private id: number = 1;
+  private duration: number = 99;
+
   constructor () {
-    console.log("my number is " + this.myNumber);
     // print to console
     this.printPerson(this.name, this.age);
+    // new product
+    let newProduct = new Product(1);
+    newProduct.printDetails();
+    // new tour
+    let newTour = new Tour(this.id, this.duration);
+    //newTour.setId(999); // 999 instead of 1
+    newTour.setPrice(99999);
+    newTour.setTitle("Trip to Taj Mahal");
+    newTour.printDetails();
+
+    let newDining = new Dining(this.id, "Shrimp Tika Mashla", 5);
+    newDining.setTitle("India Trip");
+    newDining.setPrice(11);
+    newDining.printDetails();
   }
 
   printPerson (name: string, age: number): void {
@@ -22,8 +44,66 @@ export class Lab2Component {
   }
 }
 
+// Class and inheritance
+
 class Product {
-  title: string;
-  price: number;
-  id: number;
+  private title: string;
+  private price: number;
+  private id: number;
+
+  constructor (id: number) {
+    this.id = id;
+  }
+
+  setTitle(title: string){
+    this.title = title;
+  }
+  setPrice(price: number) {
+    this.price = price;
+  }
+  setId(id: number) {
+    this.id = id;
+  }
+
+  printDetails() {
+    console.log('Printing product: ==============');
+    console.log(`Title: ${this.title}`);
+    console.log(`ID: ${this.id}`);
+    console.log(`Price: ${this.price}`);
+  }
 }
+
+class Tour extends Product {
+  private duration: number;
+
+  constructor(id: number, duration: number) {
+    super(id);
+    this.duration = duration;
+  }
+  
+  printDetails() {
+    console.log('Printing Tour: ==============');
+    super.printDetails();
+    console.log(`Duration: ${this.duration}`);
+  }
+
+}
+
+class Dining extends Product {
+  private cuisine: string;
+  private childPrice: number;
+
+  constructor(id: number, cusine: string, childPrice: number) {
+    super(id);
+    this.cuisine = cusine;
+    this.childPrice = childPrice;
+  }
+
+  printDetails() {
+    console.log("Printing Dining: ========");
+    super.printDetails();
+    console.log(`cuisine: ${this.cuisine} , child price: ${this.childPrice}`);
+  }
+}
+
+// Interfaces
